f9daaba32a0a0d9b086a0d8e8485f0819e6c980f
Eugenio Serrano <eugenioserrano@msn.com>
2021-05-08 21:59:38 -0300

    subimos la libreria highlight y algunas pruebas

---
+++ eu-genius/src/App.svelte
@@ -0,0 +1,39 @@
+
+<script>
+
+</script>
+
+<main>
+	<h1>Hola !</h1>
+
+
+<pre>
+	<code class="python">
+		
+	def miFuncion():
+		a = 1
+		print ("Hola mundo")
+
+	</code>
+</pre>
+
+<pre>
+	<code>
+<div class="editor" contenteditable="true" spellcheck="false">
+	def miFuncion():
+		a = 1
+		print ("Hola mundo")
+
+</div>
+
+</code>
+</pre>
+
+<textarea id="w3review" name="w3review" rows="4" cols="50" class="python" >		
+	def miFuncion():
+		a = 1
+		print ("Hola mundo")
+</textarea>
+
+
+</main>
